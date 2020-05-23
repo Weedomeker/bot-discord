34e58670bbcf4bd5d7308f18f4cde08199a0ad7e
Weedomeker <weedomeker@gmail.com>
2020-05-23 21:22:42 +0200

    Update

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
+const {TOKEN, PREFIX} = require("./config.js");
 const client = new Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+client.on("ready", () => {
+  console.log("Je suis OnFire !");
 });
 
-client.on('message', msg => {
-  if (msg.content === '!gvgtroll') {
-    msg.channel.send('@everyone, Ce soir TROLL DEF GvG !!! Merci.');
-    msg.channel.send('@everyone, Ce soir GROSSE DEF !!! Merci.');
-  }
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}gvg`)) msg.channel.send("@everyone, Ce soir GROSSE DEF GvG !!! Merci.");
 });
 
-client.login('NzA0NzQ5MDM5NTQyODYxODQ2.XskarA.w7wOoXZb8AH_KHVuJcoQNCOOIFA');
+client.login(TOKEN);
