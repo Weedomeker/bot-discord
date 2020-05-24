5d14ca346e31eba44cde4fcb998e666fdd3f624b
Weedomeker <weedomeker@gmail.com>
2020-05-24 14:37:49 +0200

    Add Commands ext

---
+++ commands/gvg.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'gvg',
+  description: 'Demande de placer grosse def gvg.',
+  execute(message, args) {
+    message.channel.send('@everyone, Ce soir, GROSSE DEF GvG !!! Merci.');
+  }
+}
--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-const { Client } = require("discord.js");
-const { TOKEN, PREFIX } = require("./config.js");
+const fs = require("fs");
+const { Client, Collection } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config.js');
+
 const client = new Client();
+client.commands = new Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(commandFiles);
 
-client.on("ready", () => {
-  console.log("Bot Ready...!");
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  console.log(client.commands);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", msg => {
-  if (msg.content.startsWith(`${PREFIX}gvg`)) msg.channel.send("@everyone, Ce soir GROSSE DEF GvG !!! Merci.");
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  
+  if (!client.commands.has(command)) return;
+  client.commands.get(command).execute(message, args);
+ 
 });
 
 client.login(TOKEN);
+
+
+
