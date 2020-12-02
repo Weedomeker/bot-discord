61f1e734e3ee71c0e853d63149e2f529c7cfa1e6
Weedomeker <weedomeker@gmail.com>
2020-12-02 10:40:53 +0100

    Remove useless packages

--- commands/Misc/members.js
+++ commands/Misc/members.js
@@ -1,11 +1,9 @@
-// @ts-check
 const { MessageEmbed } = require("discord.js");
 const { MESSAGES } = require("../../util/constants");
 
 module.exports.run = (client, message) => {
-  // const lantOn = message.guild.emojis.cache.get("729052532965376021");
+
   message.guild.members.fetch().then(fetchAll => {
-    // const online = fetchAll.filter(m => m.presence.status === "offline");
     const member = fetchAll.map(member => member).sort((userA, userB) => userA.joinedTimestamp - userB.joinedTimestamp);
     const user = member.map(usr => usr.user.username).join("\n");
     const embed = new MessageEmbed()
@@ -14,7 +12,6 @@ module.exports.run = (client, message) => {
       .setTimestamp()
       .setFooter(`${fetchAll.size} membres sur le serveur.`);
     message.channel.send(embed);
-    // console.log(fetchAll);
   });
 };
 
